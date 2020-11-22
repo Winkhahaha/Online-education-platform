8a51cb394f6312476eff7adc13adfff33a415112
高明 <mineok@qq.com>
2020-11-22 15:02:29 +0800

    freemarker模板基础语法练习

--- EduService-parent/test-freemarker/src/main/java/com/xuecheng/test/freemarker/controller/FreemarkerController.java
+++ EduService-parent/test-freemarker/src/main/java/com/xuecheng/test/freemarker/controller/FreemarkerController.java
@@ -71,6 +71,34 @@ public class FreemarkerController {
     public String test1(Map<String, Object> map) {
         // map就是freemaker模板所使用的数据
         map.put("name", "哈哈哈");
+        Student stu1 = new Student();
+        stu1.setName("小明");
+        stu1.setAge(18);
+        stu1.setMoney(1000.86f);
+        stu1.setBirthday(new Date());
+        Student stu2 = new Student();
+        stu2.setName("小红");
+        stu2.setMoney(200.1f);
+        stu2.setAge(19);
+        stu2.setBirthday(new Date());
+        List<Student> friends = new ArrayList<>();
+        friends.add(stu1);
+        stu2.setFriends(friends);
+        stu2.setBestFriend(stu1);
+        List<Student> stus = new ArrayList<>();
+        stus.add(stu1);
+        stus.add(stu2);
+        //向数据模型放数据
+        map.put("stus",stus);
+        //准备map数据
+        HashMap<String,Student> stuMap = new HashMap<>();
+        stuMap.put("stu1",stu1);
+        stuMap.put("stu2",stu2);
+        //向数据模型放数据
+        map.put("stu1",stu1);
+        //向数据模型放数据
+        map.put("stuMap",stuMap);
+        map.put("point",102920122);
         // 返回freemarker模板的位置:基于resources\templates目录下 -- test1.ftl
         return "test1";
     }
--- EduService-parent/test-freemarker/src/main/java/com/xuecheng/test/freemarker/model/Student.java
+++ EduService-parent/test-freemarker/src/main/java/com/xuecheng/test/freemarker/model/Student.java
@@ -17,7 +17,7 @@ public class Student {
     private String name;//姓名
     private int age;//年龄
     private Date birthday;//生日
-    private Float mondy;//钱包
+    private Float money;//钱包
     private List<Student> friends;//朋友列表
     private Student bestFriend;//最好的朋友
 }
