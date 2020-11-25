b09114c4078b16ffb5946ec22363da0694b3ffd8
高明 <mineok@qq.com>
2020-11-25 14:52:11 +0800

    producer/customer测试应用三:routing工作模式

---
+++ EduService-parent/test-rabbitMQ-consumer/src/test/java/org/mineok/test/rabbitmq/consumer/Consumer03_routing_sms.java
@@ -0,0 +1,63 @@
+package org.mineok.test.rabbitmq.consumer;
+
+import com.rabbitmq.client.*;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.concurrent.TimeoutException;
+
+/**
+ * @Author Gaoming
+ * @Email mineok@foxmail.com
+ * @Date 2020/11/24/ 22:22
+ * @Description RabbitMQ入门程序 - 消费者(发布订阅模式)_SMS
+ */
+public class Consumer03_routing_sms {
+
+    // 队列名称
+    private static final String QUEUE_INFORM_SMS = "queue_inform_sms";
+    // 交换机名称
+    private static final String EXCHANGE_ROUTING_INFORM = "exchange_routing_inform";
+    // 路由key名称
+    private static final String ROUTING_SMS = "key_inform_sms";
+
+
+    public static void main(String[] args) throws IOException, TimeoutException {
+        // 通过连接工厂和MQ建立连接
+        ConnectionFactory connectionFactory = new ConnectionFactory();
+        connectionFactory.setHost("112.126.78.213");
+        connectionFactory.setPort(5672);
+        connectionFactory.setUsername("guest");
+        connectionFactory.setUsername("guest");
+        // 设置虚拟机,一个MQ的服务可以设置多个虚拟机,每个虚拟机相当于一个独立的MQ
+        connectionFactory.setVirtualHost("/");
+        // 建立新连接
+        Connection connection = null;
+        connection = connectionFactory.newConnection();
+        // 创建会话通道,消费者和MQ服务所有通信都在channel通道中完成
+        Channel channel = connection.createChannel();
+        /* 声明队列*/
+        channel.queueDeclare(QUEUE_INFORM_SMS, true, false, false, null);
+        /* 设置一个交换机*/
+        channel.exchangeDeclare(EXCHANGE_ROUTING_INFORM, BuiltinExchangeType.DIRECT);
+        /* 队列和交换机进行绑定*/
+        channel.queueBind(QUEUE_INFORM_SMS, EXCHANGE_ROUTING_INFORM, ROUTING_SMS);
+
+        // 实现消费方法
+        DefaultConsumer defaultConsumer = new DefaultConsumer(channel) {
+            /*当接收到消息后此方法将被调用*/
+            @Override
+            public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException {
+                // 获取交换机
+                String exchange = envelope.getExchange();
+                // 消息id,MQ在通道中表示消费的id,可用于确认消息已接收
+                long deliveryTag = envelope.getDeliveryTag();
+                // 消息内容
+                String message = new String(body, StandardCharsets.UTF_8);
+                System.out.println("Consumer_routing_sms receive message -->" + message);
+            }
+        };
+        /*监听队列*/
+        channel.basicConsume(QUEUE_INFORM_SMS, true, defaultConsumer);
+    }
+}
---
+++ EduService-parent/test-rabbitMQ-producer/src/test/java/org/mineok/test/rabbitmq/producer/Producer03_routing.java
@@ -0,0 +1,112 @@
+package org.mineok.test.rabbitmq.producer;
+
+import com.rabbitmq.client.BuiltinExchangeType;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+/**
+ * @Author Gaoming
+ * @Email mineok@foxmail.com
+ * @Date 2020/11/25/ 14:06
+ * @Description RabbitMQ入门程序 - 生产者(路由模式)
+ */
+public class Producer03_routing {
+
+    // 队列名称
+    private static final String QUEUE_INFORM_EMAIL = "queue_inform_email";
+    private static final String QUEUE_INFORM_SMS = "queue_inform_sms";
+    // 交换机名称
+    private static final String EXCHANGE_ROUTING_INFORM = "exchange_routing_inform";
+    // 路由key名称
+    private static final String ROUTING_EMAIL = "key_inform_email";
+    private static final String ROUTING_SMS = "key_inform_sms";
+
+    public static void main(String[] args) throws IOException, TimeoutException {
+        // 通过连接工厂和MQ建立连接
+        ConnectionFactory connectionFactory = new ConnectionFactory();
+        connectionFactory.setHost("112.126.78.213");
+        connectionFactory.setPort(5672);
+        connectionFactory.setUsername("guest");
+        connectionFactory.setUsername("guest");
+        // 设置虚拟机,一个MQ的服务可以设置多个虚拟机,每个虚拟机相当于一个独立的MQ
+        connectionFactory.setVirtualHost("/");
+        // 建立新连接
+        Connection connection = null;
+        Channel channel = null;
+        try {
+            connection = connectionFactory.newConnection();
+            // 创建会话通道,生产者和MQ服务所有通信都在channel通道中完成
+            channel = connection.createChannel();
+            /* 声明队列,如果队列在MQ中则没必要创建
+                 参数:
+                 String queue,队列名称
+                 boolean durable,是否持久化,是则mq重启后队列还在
+                 boolean exclusive,是否独占连接(独占则只允许队列在该连接中访问,如果connection连接关闭,队列自动删除(如果此参数true可用于临时队列的创建))
+                 boolean autoDelete,队列不再使用是否自动删除此队列(如果此参数true可用于临时队列的创建)
+                 Map<String, Object> arguments 队列参数,可以设置一个队列的扩展参数,比如存活时间等
+                * */
+            channel.queueDeclare(QUEUE_INFORM_EMAIL, true, false, false, null);
+            channel.queueDeclare(QUEUE_INFORM_SMS, true, false, false, null);
+            /* 设置一个交换机
+                参数:
+                String exchange, 交换机名称
+                String type 交换机类型:
+                                fanout 对应rabbitMQ的工作模式是 publish/scribe
+                                direct 对应的Routing工作模式
+                                topic  对应的Topics工作模式
+                                headers 对应的Header工作模式
+             */
+            channel.exchangeDeclare(EXCHANGE_ROUTING_INFORM, BuiltinExchangeType.DIRECT);
+            /* 队列和交换机和路由key进行绑定
+                    参数:
+                    String queue,队列名称
+			        String exchange,交换机名称
+			        String routingKey 路由key,在发布订阅模式中设为""(交换机根据路由key将消息转发到指定的队列,发布订阅模式不需要)
+
+             */
+            channel.queueBind(QUEUE_INFORM_EMAIL, EXCHANGE_ROUTING_INFORM, ROUTING_EMAIL);
+            // 一个队列同时绑定两个routingKey
+            channel.queueBind(QUEUE_INFORM_EMAIL, EXCHANGE_ROUTING_INFORM, "test01");
+
+            channel.queueBind(QUEUE_INFORM_SMS, EXCHANGE_ROUTING_INFORM, ROUTING_SMS);
+            // 两个不同队列存在一个相同的key
+            channel.queueBind(QUEUE_INFORM_SMS, EXCHANGE_ROUTING_INFORM, "test01");
+
+
+            // 发送消息
+            /*
+                参数:
+                    String exchange,交换机,如果不指定将使用MQ的默认交换机(设置为"")
+                    String routingKey,路由key,交换机根据路由key来将消息转发到指定的队列(如果使用默认交换机,则设置为队列名称)
+			        BasicProperties props,设置消息的属性
+			        byte[] body 消息内容
+             */
+            for (int i = 0; i < 5; i++) {
+                // 发送消息时指定routingKey
+                channel.basicPublish(EXCHANGE_ROUTING_INFORM, ROUTING_EMAIL, null, "现在是路由模式".getBytes());
+                System.out.println("send inform MQ --> " + "现在是路由模式,routingKey = " + ROUTING_EMAIL);
+            }
+            for (int i = 0; i < 5; i++) {
+                // 发送消息时指定routingKey
+                channel.basicPublish(EXCHANGE_ROUTING_INFORM, ROUTING_SMS, null, "现在是路由模式".getBytes());
+                System.out.println("send inform MQ --> " + "现在是路由模式,routingKey = " + ROUTING_SMS);
+            }
+            for (int i = 0; i < 5; i++) {
+                // 发送消息时指定routingKey
+                channel.basicPublish(EXCHANGE_ROUTING_INFORM, "test01", null, "现在是路由模式".getBytes());
+                System.out.println("send inform MQ --> " + "现在是路由模式,routingKey = " + "test01");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            // 关闭通道
+            channel.close();
+            // 关闭连接
+            connection.close();
+        }
+    }
+}
